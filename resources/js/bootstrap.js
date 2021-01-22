4d1734c9d2be78f74b3cece47fb08144be325d06
dexxcrypt <adamcool180@gmail.com>
2021-01-22 20:38:39 +0700

    remove jquery popper.js bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,12 +8,6 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
-window.$ = window.jquery = require('jquery');
-
-require('bootstrap');
-
-require('popper.js');
-
 require('moment');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
